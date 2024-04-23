21ca964117a8b4d9745f58f23e5d358bc3ebcbcd
Nguyễn Văn Bình <78024720+Congchuabongbong@users.noreply.github.com>
2024-04-23 10:24:48 +0700

    feat: add app test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgxExtendedPdfViewerModule } from 'projects/ngx-extended-pdf-viewer/src/public-api';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxExtendedPdfViewerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
